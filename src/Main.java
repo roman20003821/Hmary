d3121764c59bb5d753d26c46984a4f7b9ceeb196
Roman <smakula2012@gmail.com>
2019-03-02 15:27:27 +0200

    Create class Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from 3 line");
+        System.out.println("It is me");
+        System.out.println("The only one");
+        System.out.println("The best");
+    }
+}
