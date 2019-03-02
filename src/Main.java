6565542b4b798e427da38fe1447fe3a91358adad
Valeriya <khomenets.valeriya@gmail.com>
2019-03-02 15:45:11 +0200

    Changed main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from 3 line");
-        System.out.println("It is me");
-        System.out.println("The only one");
-        System.out.println("The best");
+        System.out.println("Blahblahblah");
+        System.out.println("It is me Lera");
+        System.out.println("The only one Lera");
+        System.out.println("The best Lera");
     }
 }
